f4b1e1fcccdae8a5af3fabef5a220db74a83b1cb
Michael Trommer <mictro@gmail.com>
2018-12-09 06:41:43 +0100

    not needed

--- src/helper.js
+++ src/helper.js
@@ -11,36 +11,4 @@ export default {
         }
     }
 
-    ,
-
-    momentHumanize: function(eventDuration, unit) {
-    var eventMDuration = moment.duration(eventDuration, unit);
-    var eventDurationArray = [];
-    if (eventMDuration.years() > 0) {
-        eventDurationArray.push(eventMDuration.years() + ' years');
-        eventMDuration.subtract(eventMDuration.years(), 'years')
-    }
-    if (eventMDuration.months() > 0) {
-        eventDurationArray.push(eventMDuration.months() + ' months');
-        eventMDuration.subtract(eventMDuration.months(), 'months')
-    }
-    if (eventMDuration.weeks() > 0) {
-        eventDurationArray.push(eventMDuration.weeks() + ' weeks');
-        eventMDuration.subtract(eventMDuration.weeks(), 'weeks')
-    }
-    if (eventMDuration.days() > 0) {
-        eventDurationArray.push(eventMDuration.days() + ' days');
-        eventMDuration.subtract(eventMDuration.days(), 'days')
-    }
-    if (eventMDuration.hours() > 0) {
-        eventDurationArray.push(eventMDuration.hours() + ' hours');
-        eventMDuration.subtract(eventMDuration.hours(), 'hours')
-    }
-    if (eventMDuration.minutes() > 0) {
-        eventDurationArray.push(eventMDuration.minutes() + ' minutes');
-    }
-    return eventDurationArray.length === 1 ? eventDurationArray[0] : 
-    eventDurationArray.join(' and ')
-}
-
 }
